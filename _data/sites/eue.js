92d995cfd5ab0c998936f3d92d74afc32cd6c750
Andreas Parusel <andreas.parusel@1und1.de>
2020-12-11 14:28:32 +0100

    new arrangement

--- _data/sites/eue.js
+++ _data/sites/eue.js
@@ -5,8 +5,8 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-    "https://www.1und1.de/",
     "https://www.1und1.de/kwk",
-    "https://www.1und1.de/Kontakt"
+    "https://www.1und1.de/Kontakt",
+    "https://www.1und1.de/login",
 	]
 };
--- _data/sites/startseiten.js
+++ _data/sites/startseiten.js
@@ -6,8 +6,6 @@ module.exports = {
 	},
 	urls: [
 		"https://www.1und1.de/",
-		"https://mobile.1und1.de/",
-		"https://dsl.1und1.de/",
 		"https://www.premiumsim.de/",
 		"https://www.telekom.de/start",
 		"https://www.vodafone.de/",
