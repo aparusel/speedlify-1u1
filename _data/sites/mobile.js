4971d401db963d5797b1b26049475192c3d8c33d
Andreas Parusel <andreas.parusel@1und1.de>
2020-12-11 13:44:54 +0100

    added 1&1 sites

---
+++ _data/sites/mobile.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "Mobile", // optional, falls back to object key
+	description: "Mobile",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+    "https://mobile.1und1.de/",
+    "https://mobile.1und1.de/mobilfunktarife",
+    "https://mobile.1und1.de/smartphones",
+    "https://mobile.1und1.de/tablets"
+	]
+};
---
+++ _data/sites/startseiten.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "Startseiten", // optional, falls back to object key
+	description: "Startseiten",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.1und1.de/",
+		"https://mobile.1und1.de/",
+		"https://dsl.1und1.de/",
+		"https://www.premiumsim.de/",
+		"https://www.telekom.de/start",
+		"https://www.vodafone.de/",
+		"https://www.o2online.de/",
+		"https://www.ionos.de/",
+		"https://www.strato.de/",
+	]
+};
